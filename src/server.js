4cb751603a9bca22b4b810a1381650b1937817af
ShodiyevF <fayzulloh6757@gmail.com>
2024-04-12 21:51:10 +0500

    refactor server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 require('dotenv').config()
 
-const bot = new TelegramBot(process.env.bot_token, { polling: true })
-const { cleanDirectory } = require('./modules/cleanDirectory');
 const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(process.env.bot_token, { polling: true, baseApiUrl: 'http://localhost:8081' })
+const { uploadsInitFolder } = require('./config/default.folders');
+const { cleanDirectory } = require('./modules/cleanDirectory');
+const { importDbRunner } = require('./modules/importDbNs');
+const { dbStatInitFile } = require('./config/db_stat');
 
-// importDbRunnerNasiyasavdo(bot)
-// importDbRunnerElmakon(bot)
-// cleanDirectory()
+async function initializer() {
+    uploadsInitFolder()
+    dbStatInitFile()
+
+    importDbRunner(bot)
+    cleanDirectory()
+}
+
+
+initializer()
