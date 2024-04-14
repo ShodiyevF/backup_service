f46ac59c2a68fa36ff17a224a8007c9ab3b309ec
ShodiyevF <fayzulloh6757@gmail.com>
2024-04-15 00:41:30 +0500

    fix server.js

--- src/server.js
+++ src/server.js
@@ -3,15 +3,15 @@ require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.bot_token, { polling: true, baseApiUrl: 'http://localhost:8081' })
 const { uploadsInitFolder } = require('./config/default.folders');
-const { cleanDirectory } = require('./modules/cleanDirectory');
-const { importDbRunner } = require('./modules/importDbNs');
+const { cleanDirectory } = require('./config/cleanDirectory');
+const { backupRunner } = require('./modules/backup.runner');
 const { dbStatInitFile } = require('./config/db_stat');
 
 async function initializer() {
     uploadsInitFolder()
     dbStatInitFile()
 
-    importDbRunner(bot)
+    backupRunner(bot)
     cleanDirectory()
 }
 
