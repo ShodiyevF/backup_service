0a28216bef46620a365d3395e69b833081d36383
Nasiyasavdo <nasiyasavdodeveloper@gmail.com>
2024-04-16 17:46:11 +0500

    change server

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.bot_token, { polling: true, baseApiUrl: 'http://localhost:8081' })
 const { initializeFolder } = require('./config/default.folders');
 const { cleanDirectory } = require('./config/cleanDirectory');
-const { backupRunner } = require('./modules/backup.runner');
 const { dbStatInitFile } = require('./config/db_stat');
+const { backupRunner } = require('./backup.runner');
 
 async function initializer() {
     initializeFolder()
